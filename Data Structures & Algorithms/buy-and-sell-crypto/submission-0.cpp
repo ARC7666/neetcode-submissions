499165d9fb9622b7ee13a6a9cafc2b7c391a4010
Ankit Ranjan <ankitranjan766@gmail.com>
2026-07-23 12:23:29 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = INT_MAX;
+        int maxprice = 0;
+
+        for(auto p : prices){
+            minprice = min(minprice , p);
+            maxprice = max(maxprice, p - minprice);
+        }
+
+        return maxprice;
+    }
+};
